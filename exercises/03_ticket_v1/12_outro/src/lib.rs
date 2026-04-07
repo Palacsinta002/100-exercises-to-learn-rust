856a62747edf70af7d0026ac02f6c0561b5720bf
Vágvölgyi Máté <h593110@stud.u-szeged.hu>
2026-04-07 13:10:15 +0200

    Completed 03_12_outro

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -5,6 +5,63 @@
 //   The unit price is in cents and must be strictly greater than zero.
 //   Order must include a method named `total` that returns the total price of the order.
 //   Order must provide setters and getters for each field.
+
+pub struct Order {
+    product_name: String,
+    quantity: u16,
+    unit_price: u16
+}
+
+impl Order{
+    pub fn new(product_name: String, quantity: u16, unit_price: u16) -> Order{
+        validate(&product_name, &quantity, &unit_price);
+        Order {
+            product_name,
+            quantity,
+            unit_price
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &u16 {
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &u16 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String){
+        validate(&product_name, &self.quantity, &self.unit_price);
+        self.product_name = product_name;
+    }
+    pub fn set_quantity(&mut self, quantity: u16){
+        validate(&self.product_name, &quantity, &self.unit_price);
+        self.quantity = quantity;
+    }
+    pub fn set_unit_price(&mut self, unit_price: u16){
+        validate(&self.product_name, &self.quantity, &unit_price);
+        self.unit_price = unit_price;
+    }
+
+    pub fn total(&self) -> u16 {
+        self.quantity * self.unit_price
+    }
+}
+
+fn validate(product_name: &String, quantity: &u16, unit_price: &u16){
+    if product_name.is_empty() || product_name.len() > 300 {
+        panic!("Product name must not be empty or more than 300 bytes long!");
+    }
+    if quantity <= &0 {
+        panic!("Quantity must be greater than 0!")
+    }
+    if unit_price <= &0 {
+        panic!("Unit price must be greater than 0!")
+    }
+}
+
 //
 // Tests are located in a different place this time—in the `tests` folder.
 // The `tests` folder is a special location for `cargo`. It's where it looks for **integration tests**.
